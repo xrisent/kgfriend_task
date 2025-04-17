dbd6025142d0b9a3a3446db4870045f7c4923068
xrisent <ormonov31261@gmail.com>
2025-04-17 11:52:12 +0600

    made redirect on click on card, also changed interface for User

--- src/components/UserCard/UserCard.tsx
+++ src/components/UserCard/UserCard.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { User } from '../../entities/User';
 import './UserCard.scss'
 
 const UserCard: React.FC<{ user: User }> = ({ user }) => {
+    const navigate = useNavigate()
+
+    const handleClick = (()=>{
+      navigate(`/users/${user.id}`)
+    })
+
     return (
-      <div className='userCard'>
+      <div className='userCard' onClick={handleClick}>
         <h3>{user.name}</h3>
         <p>{user.email}</p>
         <p>{user.address.city}</p>
-        <Link to={`/users/${user.id}`}>Подробнее</Link>
       </div>
     );
   };
